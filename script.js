4e8ba9729d119d361b3518af27fceebfae0c9033
kienquoc2309 <kienquocthk9@gmail.com>
2023-11-25 18:55:49 +0700

    UI infomation vs UI report

---
+++ script.js
@@ -0,0 +1,23 @@
+"use strict"
+const pencil = document.querySelector('.ti-pencil');
+console.log(pencil)
+const inputs = document.querySelectorAll('.input');
+console.log(inputs)
+const span2 = document.querySelectorAll('.span2');
+console.log(span2)
+
+const hidespan2 = function () {
+    span2.forEach(span => span.classList.toggle('hide'))
+}
+const updatespan = function () {
+    inputs.forEach((input, i) => span2[i].textContent = input.value)
+}
+const showinput = function () {
+    inputs.forEach(input => input.classList.toggle('hide'))
+}
+pencil.addEventListener('click', (e) => {
+    e.preventDefault();
+    hidespan2();
+    showinput();
+
+})
