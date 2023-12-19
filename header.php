ae161b893de49d33476a0ace0810eeeba8a6a1bd
kienquoc2309 <kienquocthk9@gmail.com>
2023-12-19 23:46:44 +0700

    file header(require file)

---
+++ header.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+require_once ('database/connect.php');
+require_once ('database/database.php');
+require_once ('session/session.php');
+require_once ('function.php');
+
+
+?>
