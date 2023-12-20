a7f6baa06e1229e841ff4681e1ddb26534fe6788
kienquoc2309 <kienquocthk9@gmail.com>
2023-12-20 16:19:06 +0700

    update edit

--- edit.php
+++ edit.php
@@ -171,17 +171,57 @@ $error = getFlashData('error');
 
                 </div>
                 <div class="div_input">
-                    <input type="text" name="job" id="job" placeholder="Job" class="info_input" value = "<?php echo $job ?>">
+                    <span class = "span_gender">Gender:</span>
+                    <div class="div_gender">
+                        <input type="radio" name="gender" id="male" value ="Male" class="gender_input">
+                        <label class ="label_gender" for="male">Male</label>
+                    </div>
+                    <div class="div_gender">
+                        <input type="radio" name="gender" id="female" value ="Female" class="gender_input">
+                        <label class ="label_gender" for="female">Female</label>
+                    </div>
                 </div>
                 <div class="div_input">
-                    <input type="text" name="gender" id="gender" placeholder="Gender(Female/Male)" class="info_input" value = "<?php echo $gender ?>">
+                <label for="job" class = "label_input">Select Job:</label>
+                <select name="job" class ="info_input select_input" id="job" >
+                    <option value="Nghiên cứu">Nghiên cứu</option>
+                    <option value="Kinh doanh">Kinh doanh</option>
+                    <option value="Văn phòng">Văn phòng</option>
+                    <option value="Sáng tạo">Sáng tạo</option>
+                </select>
+                </div>
+                <div class="div_input">
+                <label for="location" class = "label_input">Select Location:</label>
+                <select name="location" class ="info_input select_input" id="location" value = "<?php echo $location ?>" >
+                    <option value="HCM">HCM</option>
+                    <option value="HN">HN</option>
+                    <option value="DN">DN</option>
+                    <option value="CT">CT</option>
+                </select>
+                </div>
+                <div class="div_input">
+                     <span class = "span_gender span_hobby">Hobby:</span>
+                     <div class="div_checkbox">
+                        <input type="checkbox" name="hobby[]" id="bongda" class="checkbox_input" value ="Bóng đá" >
+                        <label for="bongda" class= "label_checkbox">Bóng đá</label>
+                     </div>
+                     <div class="div_checkbox">
+                        <input type="checkbox" name="hobby[]" id="bongro" class="checkbox_input" value ="Bóng rổ" >
+                        <label for="bongro" class= "label_checkbox">Bóng rổ</label>
+                     </div>
+                     <div class="div_checkbox">
+                        <input type="checkbox" name="hobby[]" id="bongchuyen" class="checkbox_input" value ="Bóng chuyền" >
+                        <label for="bongchuyen" class= "label_checkbox">Bóng chuyền</label>
+                     </div>
+                     <div class="div_checkbox">
+                        <input type="checkbox" name="hobby[]" id="caulong" class="checkbox_input" value ="Cầu lông" >
+                        <label for="caulong" class= "label_checkbox">Cầu lông</label>
+                     </div>
+                     <div class="div_checkbox">
+                        <input type="checkbox" name="hobby[]" id="boiloi" class="checkbox_input" value ="Bơi lội" >
+                        <label for="boiloi" class= "label_checkbox">Bơi lội</label>
+                     </div>
 
-                </div>
-                <div class="div_input">
-                    <input type="text" name="location" id="location" placeholder="Location" class="info_input" value = "<?php echo $location ?>">
-                </div>
-                <div class="div_input">
-                     <input type="text" name="hobby" id="hobby" placeholder="Hobby" class="info_input" value = "<?php echo $hobby ?>">
                 </div>
                 <div class="div_input">
                     <input type="submit" name="submit" value="Enter" class="submit info_input"> 
